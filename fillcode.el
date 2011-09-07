d785361c43a56afe3c9fc542b39ed58128c7792c
ryanb <ryanb@28b174d2-27d0-0310-a1ca-f369082887e2>
2011-09-07 00:34:11 +0000

    tweak comment

--- fillcode.el
+++ fillcode.el
@@ -485,7 +485,8 @@ point to next non-whitespace char."
   (let ((fill-point-re (build-re fillcode-fill-points)))
   (cond
    ; if we're in a string literal or comment, add a space before it (unless this
-   ; is a keyword arg in a function call, e.g. in python), then skip to the end of it.
+   ; is a keyword arg in a function call, e.g. in python), then skip to the end
+   ; of it.
    ((fillcode-in-literal)
     (when (save-excursion (backward-char)
                           (and (not (fillcode-in-literal)) (not (looking-at "="))))
